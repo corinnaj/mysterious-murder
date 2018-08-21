73e46c847046f23e1eb69945b1a45edb70804bba
Corinna Jaschek <corinna.jaschek@gmail.com>
2018-08-21 23:59:11 +1000

    start generating init

---
+++ init_generator.py
@@ -0,0 +1,115 @@
+import random
+
+characters = []
+
+def createCharacters(number):
+    result = ''
+    for x in range(65, 65 + number):
+        result += 'existsC '
+        result += chr(x)
+        characters.append(chr(x))
+        result += ',\n'
+    return result
+
+def createFamilies():
+    result = ''
+    for i in range(1, len(characters)):
+        for j in range(i + 1, len(characters)):
+            if random.random() < 0.2:
+                result += print_sym_pred('related', characters[i], characters[j])
+            else:
+                result += print_sym_pred('not_related', characters[i], characters[j])
+                rand = random.random()
+                if rand < 0.25:
+                    result += print_sym_pred('married', characters[i], characters[j]) 
+                elif rand < 0.4:
+                    result += print_sym_pred('lovers', characters[i], characters[j]) 
+    return result
+
+def createTraits():
+    result = ''
+    for c in characters:
+        r = random.randrange(3)
+        result += print_trait_balance('naive', 'cunning', r, 3 - r, c)
+        r = random.randrange(3)
+        result += print_trait_balance('loyal', 'cunning', r, 3 - r, c)
+
+        r = random.randrange(3)
+        result += print_trait('paranoia', r, c)
+        r = random.randrange(3)
+        result += print_trait('sadness', r, c)
+    return result
+
+def createRelationships():
+    result = ''
+    for i in range(1, len(characters)):
+        for j in range(i + 1, len(characters)):
+            r = random.randrange(3)
+            result += print_relationship('affection', r, characters[i], characters[j])
+            r = random.randrange(3)
+            result += print_relationship('anger', r, characters[i], characters[j])
+    return result
+
+def print_relationship(name, amount, character1, character2):
+    result = ''
+    for i in range(0, amount):
+        result += print_pred_two(name, character1, character2)
+    result += '\n'
+    return result
+
+def print_trait(name, amount, character):
+    result = ''
+    for i in range(0, amount):
+        result += print_pred_one(name, character)
+    result += '\n'
+    return result
+
+def print_trait_balance(a, b, numA, numB, character):
+    result = ''
+    result += print_trait(a, numA, character)
+    result += print_trait(b, numB, character)
+    return result
+
+def print_pred_one(name, character):
+    result = ''
+    result += name
+    result += ' '
+    result += character
+    result += ', '
+    return result
+
+def print_pred_two(name, character1, character2):
+    result = ''
+    result += name
+    result += ' '
+    result += character1
+    result += ' '
+    result += character2
+    result += ', '
+    return result
+
+def print_sym_pred(name, a, b):
+    result = ''
+    result += name
+    result += ' '
+    result += a
+    result += ' '
+    result += b
+    result += ', '
+    result += name
+    result += ' '
+    result += b
+    result += ' '
+    result += a
+    result += ',\n'
+    return result
+
+with open('init', 'w') as f:
+    f.write('context init = {\n')
+    f.write(createCharacters(5));
+    f.write(createFamilies())
+    f.write(createTraits())
+    f.write(createRelationships())
+    f.write('}.')
+
+print('init created')
