b5614a453a054a3b47e6e9f4def831d2f250d7f3
Tom Beckmann <tomjonabc@gmail.com>
2018-08-31 12:36:09 +1000

    show all the colors

--- src/evaluator.py
+++ src/evaluator.py
@@ -175,7 +175,7 @@ class GraphPrinter:
     def __init__(self, evaluator, view=False, show_all=False):
         color_inc = 1 / len(evaluator.actors)
         self.actor_colors = {evaluator.actors[i]:
-                             '{} 0.2 1.0'.format(str(color_inc * i))
+                             '{} s 1.0'.format(str(color_inc * i))
                              for i in range(len(evaluator.actors))}
 
         self.dot = Digraph(format='svg')
@@ -183,13 +183,17 @@ class GraphPrinter:
         self.print_instances(evaluator.init_state, show_all=show_all)
         self.dot.render('output', view=view)
 
+    def color_list(self, list, saturation):
+        return ':'.join([self.actor_colors[actor].replace('s', str(saturation))
+                         for actor in list])
+
     def print_instances(self, instances, parent=None, show_all=False):
         for instance in instances:
             if instance.consumed_by or show_all:
                 self.dot.attr('node',
-                              style='filled',
-                              fillcolor=self.actor_colors[instance.actors[0]],
-                              shape='ellipse')
+                              style='striped',
+                              fillcolor=self.color_list(instance.actors, 0.1),
+                              shape='box')
                 self.dot.node(instance.id, str(instance))
                 if parent:
                     self.dot.edge(parent.id, instance.id)
@@ -203,8 +207,8 @@ class GraphPrinter:
     def print_rule(self, rule, show_all=False):
         self.existing_rules.add(rule.id)
         self.dot.attr('node',
-                style='filled',
-                shape='box',
-                fillcolor=self.actor_colors[rule.args[0]])
+                      style='striped',
+                      shape='box',
+                      fillcolor=self.color_list(rule.args, 0.4))
         self.dot.node(rule.id, str(rule))
         self.print_instances(rule.produced, rule, show_all=show_all)
